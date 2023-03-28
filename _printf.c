96c456e9a823e6edf9e2788fc4925130689c0e3b
Mark <marknjoropge@gmail.com>
2023-03-28 14:18:54 -0700

    Tried adding support for i and d

--- _printf.c
+++ _printf.c
@@ -10,45 +10,85 @@
  * @format: Format string
  * Return: Number of characters printed
  */
+
+int print_number(int n);
+
 int _printf(const char *format, ...)
 {
 va_list args;
 int count;
 char c;
 char *s;
+int i;
 count = 0;
+
 if (format == NULL)
-return (-1);
+	return (-1);
 va_start(args, format);
+
 while (*format)
 {
 if (*format == '%')
 {
-format++;
-if (*format == '\0')
-return (-1);
-if (*format == 'c')
-{
-c = va_arg(args, int);
-count += write(1, &c, 1);
-}
-else if (*format == 's')
-{
-s = va_arg(args, char *);
-count += write(1, s, strlen(s));
-}
-else if (*format == '%')
-count += write(1, "%", 1);
+	format++;
+	if (*format == '\0')
+		return (-1);
+	if (*format == 'c')
+	{
+		c = va_arg(args, int);
+		count += write(1, &c, 1);
+	}
+	else if (*format == 's')
+	{
+		s = va_arg(args, char *);
+		count += write(1, s, strlen(s));
+	}
+	 if (*format == 'i')
+	{
+		i = va_arg(args, int);
+		printf("---TEST--");
+		count += print_number(i);
+	}
+	else if (*format == 'd')
+	{
+		i = va_arg(args, int);
+		count += print_number(i);	
+	}
+	else if (*format == '%')
+                count += write(1, "%", 1);
+	else
+		count += write(1, "%", 1) + write(1, &*format, 1);
+	}
 else
-count += write(1, "%", 1) + write(1, &*format, 1);
-}
-else
 {
-count += write(1, &*format, 1);
+	count += write(1, &*format, 1);
 }
-format++;
+	format++;
 }
 write(1, '\0', 1);
 va_end(args);
 return (count);
 }
+
+int print_number(int n)
+{
+  int count = 0;
+  char digit;
+
+  if (n < 0)
+  {
+    count += write(1, "-", 1);
+    n *= -1;
+  }
+
+  if (n / 10)
+  {
+    count += print_number(n / 10);
+  }
+
+  digit = (n % 10) + '0';
+  count += write(1, &digit, 1);
+
+  return count;
+}
+
